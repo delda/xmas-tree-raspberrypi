e54898a406e11c74326ae5d7dda67d827011fb8b
Davide Dell'Erba <davide.dellerba@skuola.net>
2021-11-09 00:54:28 +0100

    Added two new options: - rgb - all colors

--- main.py
+++ main.py
@@ -4,17 +4,11 @@ import sys
 from colorzero import Hue, Color
 from random import randint, random, choice
 
+TOP_LED = 3
+
 tree = RGBXmasTree()
 tree.brightness = 0.05
 
-def main(tree):
-    switch_case = {
-        1: red_green_blue,
-    }
-    while True:
-        duration = randint(5, 20)
-        current = randint(1, len(switch_case))
-        switch_case[current](duration)
 
 def red_green_blue(duration):
     colors = [Color('red'), Color('green'), Color('blue')]
@@ -22,13 +16,28 @@ def red_green_blue(duration):
     while seconds < duration:
         for color in colors:
             tree.color = color
+            tree[TOP_LED].color = Color('white')
             sleep(1)
             seconds += 1
 
+
+
+def main():
+    switch_case = {
+        1: red_green_blue,
+        2: all_colors,
+        3: sparkle,
+    }
+    while True:
+        duration = randint(5, 20)
+        current = randint(1, len(switch_case))
+        current = 3
+        switch_case[current](duration)
+
+
 try:
-    main(tree)
+    main()
     tree.off()
 except KeyboardInterrupt:
     tree.off()
     sys.exit(0)
-
